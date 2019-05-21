c3ee2a1422004229a08f7400302371703da6e288
ciureancatalin <cataciurean@rocketmail.com>
2019-05-21 17:58:29 +0300

    notification service (vizualizare notificari in functie de userID)

---
+++ jbugs-business/src/main/java/msg/exeptions/RuntimeExceptionMapper.java
@@ -0,0 +1,19 @@
+package msg.exeptions;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * Document me.
+ *
+ * @author msg systems AG; User Name.
+ * @since 19.1.2
+ */
+@Provider
+public class RuntimeExceptionMapper implements ExceptionMapper<RuntimeException> {
+    @Override
+    public Response toResponse(RuntimeException e) {
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("UnexpectedErrorOccurred").build();
+    }
+}
