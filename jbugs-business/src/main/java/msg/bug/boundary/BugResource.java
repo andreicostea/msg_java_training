37f8eadf762b595f77dc6ddeeb26034d033d5b70
Pamela Hrituc <pam997.PH@gmail.com>
2019-05-21 18:47:34 +0300

    getAll for Bug, v1.1

--- jbugs-business/src/main/java/msg/bug/boundary/BugResource.java
+++ jbugs-business/src/main/java/msg/bug/boundary/BugResource.java
@@ -1,10 +1,10 @@
 package msg.bug.boundary;
 
+import msg.bug.entity.dto.BugDTO;
+
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -14,21 +14,27 @@ public class BugResource {
     @EJB
     private BugFacade facade;
 
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createBug(BugDTO input)
+    {
+        facade.createBug(input);
+        return Response.ok().build();
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response updateBug(BugDTO input)
+    {
+        //facade.updateBug(input);
+        return Response.ok().build();
+    }
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAll()
     {
         return Response.status(200).entity(facade.getAll()).build();
-//        return Response
-//                .status(200)
-//                .header("Access-Control-Allow-Origin", "*")
-//                .header("Access-Control-Allow-Credentials", "true")
-//                .header("Access-Control-Allow-Headers",
-//                        "origin, content-type, accept, authorization")
-//                .header("Access-Control-Allow-Methods",
-//                        "GET, POST, PUT, DELETE, OPTIONS, HEAD")
-//                .entity(facade.getAll())
-//                .build();
     }
 
 }
