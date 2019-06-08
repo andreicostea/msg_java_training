8f91465821d092b5564c7ccf664c1efb7e21a0b0
Adrian <adrian.todirica7@hotmail.com>
2019-06-09 01:46:17 +0300

    Added change password frontend

---
+++ jbugs-angular/src/app/features/view-profile/components/change-password/change-password.component.ts
@@ -0,0 +1,44 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from "@angular/forms";
+import {ErrorStateMatcher} from "@angular/material";
+import {UsersService} from "../../../users/services/users.service";
+
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const invalidCtrl = !!(control && control.invalid && control.parent.dirty);
+    const invalidParent = !!(control && control.parent && control.parent.invalid && control.parent.dirty);
+
+    return (invalidCtrl || invalidParent);
+  }
+}
+
+@Component({
+  selector: 'app-change-password',
+  templateUrl: './change-password.component.html',
+  styleUrls: ['./change-password.component.css']
+  // providers: [UsersService]
+})
+export class ChangePasswordComponent {
+
+  changePasswordForm: FormGroup;
+  matcher = new MyErrorStateMatcher();
+
+  constructor(private formBuilder: FormBuilder, private userService: UsersService) {
+    this.changePasswordForm = this.formBuilder.group({
+      password: ['', [Validators.required]],
+      confirmPassword: ['']
+    }, {validator: this.checkPasswords});
+  }
+
+  private checkPasswords(group: FormGroup) { // here we have the 'passwords' group
+    let pass = group.controls.password.value;
+    let confirmPass = group.controls.confirmPassword.value;
+
+    return pass === confirmPass ? null : { notSame: true }
+  }
+
+  onSubmit() {
+    this.userService.
+  }
+}
--- jbugs-angular/src/app/features/view-profile/view-profile.module.ts
+++ jbugs-angular/src/app/features/view-profile/view-profile.module.ts
@@ -3,12 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { ViewProfileRoutingModule } from './view-profile-routing.module';
 import { ViewProfileComponent } from './components/view-profile/view-profile.component';
+import { ChangePasswordComponent } from './components/change-password/change-password.component';
+import {MatButtonModule, MatInputModule} from "@angular/material";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
-  declarations: [ViewProfileComponent],
+  declarations: [ViewProfileComponent, ChangePasswordComponent],
   imports: [
     CommonModule,
-    ViewProfileRoutingModule
+    ViewProfileRoutingModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule
   ]
 })
 export class ViewProfileModule { }
